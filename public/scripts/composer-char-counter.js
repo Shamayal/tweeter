bbb5b3db1f37bdea0f01c2dc9e621dc58dc903e6
Shamayal Syed <shamayalsyed9@gmail.com>
2023-04-27 02:05:34 -0400

    character limit class added to change counter colour to red

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,10 +2,16 @@
 
 $(document).ready(function() {
   $("#tweet-text").on("keyup", function() {
-    let charLgeft = 140 - this.value.length;
+    let charLeft = 140 - this.value.length;
 
-    let $counter = $(this).parent(".form-bar").find(".counter");
-    $counter.text(charLeft);
-    console.log
+    let counter = $(this).parent(".form-bar").find(".counter");
+    counter.text(charLeft);
+
+    if (charLeft < 0) {
+      counter.addClass("exceed-limit");
+    } else {
+      counter.removeClass("exceed-limit");
+    }
+
   });
 });
