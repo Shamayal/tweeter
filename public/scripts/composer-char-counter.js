8add0cc99ce4b3b0097fd49927edd83ecbb89039
Shamayal Syed <shamayalsyed9@gmail.com>
2023-04-29 00:30:36 -0400

    comments for function

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -4,9 +4,11 @@ $(document).ready(function() {
   $("#tweet-text").on("keyup", function() {
     let charLeft = 140 - this.value.length;
 
+    // updates the character count value to the counter class
     let counter = $(this).parent(".form-bar").find(".counter");
     counter.text(charLeft);
 
+    // creates a new class for errors to change the color of the counter
     if (charLeft < 0) {
       counter.addClass("exceed-limit");
     } else {
